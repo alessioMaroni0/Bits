55405e61fb08ce0624c7d609304ce8dd3e3abf25
alessio <ale@deb-ale.deb-ale>
2026-07-03 02:47:40 -0700

    OK_SUM and OK_SUB for signed and unsigned 32 type

--- bits.h
+++ bits.h
@@ -5,8 +5,16 @@
 #define INT_MAX (int)(UINT_MAX >> 1)
 #define INT_MIN (-INT_MAX - 1)
 
-#define OK_SUM32(x,y)\
+#define OK_SUM_I32(x, y) \
     (!(((y) > 0 && (x) > INT_MAX - (y)) || ((y) < 0 && (x) < INT_MIN - (y))))
 
+#define OK_SUM_U32(x, y) \
+    (!((x) > UINT_MAX - (y)))
+
+#define OK_SUB_I32(x, y) \
+    (!(((y) < 0 && (x) > INT_MAX + (y)) || ((y) > 0 && (x) < INT_MIN + (y))))
+
+#define OK_SUB_U32(x, y) \
+    ((x) >= (y))
 
 #endif
