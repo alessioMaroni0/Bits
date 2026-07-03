0cd027f1af18887b252e6974878f2f61991ad058
alessio <ale@deb-ale.deb-ale>
2026-07-03 03:09:33 -0700

    Edian swap

--- bits.h
+++ bits.h
@@ -18,12 +18,18 @@
     ((x) >= (y))
 
 #define REVERSE(x) (~(x))
-
 #define BIT_SET(x, n) ((x) |= (1ULL << (n)))
 #define BIT_CLEAR(x, n)  ((x) &= ~(1ULL << (n)))
 #define BIT_TOGGLE(x, n) ((x) ^= (1ULL << (n)))
 #define BIT_CHECK(x, n)  (((x) >> (n)) & 1ULL)
 #define SET_ALL_ONE(x) ((x) = ~0)
 #define SET_ALL_ZERO(x) ((x) = 0)
+
+#define SWAP_ENDIAN32(x) ( \
+    (((x) & 0x000000FFU) << 24) | \
+    (((x) & 0x0000FF00U) << 8)  | \
+    (((x) & 0x00FF0000U) >> 8)  | \
+    (((x) & 0xFF000000U) >> 24)   \
+)
 
 #endif
