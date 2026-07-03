8cdb7a15c801b7674bb8ad154fcd900358339c67
alessio <ale@deb-ale.deb-ale>
2026-07-03 11:41:25 -0700

    multiplication e divison

--- lib/checker/checker.h
+++ lib/checker/checker.h
@@ -15,4 +15,21 @@
 #define OK_SUB_U32(x, y) \
     ((x) >= (y))
 
+#define Ok_MUL_I32(x, y) ( \
+    (x) == 0 || (y) == 0 ? 1 : ( \
+    (x) > 0 && (y) > 0 ? (x) <= INT32_MAX / (y) : ( \
+    (x) < 0 && (y) < 0 ? (x) >= INT32_MAX / (y) : ( \
+    (x) > 0 ? (y) >= INT32_MIN / (x) : (x) >= INT32_MIN / (y) \
+    ))))
+
+#define Ok_MUL_U32(x, y) \
+    ((x) > 0 && (y) > 0 && a > I32_MAX / y)
+
+#define Ok_DIV_I32(x, y) ( \
+    (y) == 0 ? 0 : ( \
+    (x) == INT32_MIN && (y) == -1 ? 0 : 1 \
+    ))
+
+#define Ok_DIV_U32(x, y) ((y) != 0)
+
 #endif
